dd75a00d875e64f4d54c4201c27fe10d47dd0d8b
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2022-02-07 00:46:54 -0300

    asd

--- components/Home/Presentation.jsx
+++ components/Home/Presentation.jsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 export default function Presentation() {
     return (   
         <>
@@ -8,20 +10,20 @@ export default function Presentation() {
                 <span>Web Developer</span>
                 <hr />
                 <div className="resoom">
-                    <a href="" className="link">
+                    <a href="/files/Santillan.pdf" download className="link">
                     <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-file" width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#000000" fill="none" strokeLinecap="round" strokeLinejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                         <path d="M14 3v4a1 1 0 0 0 1 1h4" />
                         <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" />
                     </svg>Resume
                     </a>
-                    <a href="" className="link">
+                    <a href="https://www.github.com/nawesan12" target="_blank" rel="noreferrer" className="link">
                     <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-github" width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#000000" fill="none" strokeLinecap="round" strokeLinejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                         <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
                     </svg>Github
                     </a>
-                    <a href="" className="link">
+                    <a href="https://www.linkedin.com/in/nahuel-santillan" target="_blank" rel="noreferrer" className="link">
                     <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-linkedin" width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#000000" fill="none" strokeLinecap="round" strokeLinejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                         <rect x="4" y="4" width="16" height="16" rx="2" />
@@ -34,10 +36,13 @@ export default function Presentation() {
                 </div>
             </article>
             <article className="myimage">
-                aca va mi foto
+                <div className="image-container">
+                    <Image alt="Santillan Nahuel Picture" src="/images/penguin.png" layout="fill" object-fit="cover"/>
+                </div>
             </article>
         </section>
         <section className="contact-ways">
+
         </section>
 
 
@@ -114,8 +119,8 @@ export default function Presentation() {
             }
 
             .myimage {
-                width:35rem;
-                height:35rem;
+                width:70vh;
+                height:70vh;
                 background: linear-gradient(to bottom right, #642b73, #c6426e);
                 opacity:.8;
                 border-radius: 65% 35% 59% 41% / 40% 67% 33% 60%;
@@ -125,12 +130,31 @@ export default function Presentation() {
                 align-items:center;
             }
 
+            .image-container {
+                height:90%;
+                width:90%;
+                position:relative;
+            }
+
             .contact-ways {
                 height:90vh;
                 width:100vw;
             }
 
-            
+            @media screen and (max-width:1200px) {
+               .presentation {
+                    height:120vh;
+                    padding:0;
+                    flex-direction:column;
+                    border-radius: 0 0 4rem 4rem;
+                    justify-content:space-evenly;
+                }
+
+                .myimage {
+                    height:45vh;
+                    width:45vh;
+                }
+            }
                 
         `}</style>
         </>
--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -14,11 +14,19 @@ export default function Layout({ children }) {
     return(
         <>
         <header className="app-header">
+            <div className="burger" onClick={() => setNavOpen(!isOpen)}>
+                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-menu-2" width="45" height="45" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#000000" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <line x1="4" y1="6" x2="20" y2="6" />
+                    <line x1="4" y1="12" x2="20" y2="12" />
+                    <line x1="4" y1="18" x2="20" y2="18" />
+                </svg>
+            </div>
             <div className="logo">
                 <h2>NS Dev</h2>
             </div>
             <nav className={isOpen === true ? "navigation visible" : "navigation"}>
-                <ul className="list">
+                <div className="list">
                     <li className="item">
                         <Link href="/"><a href="#">Home</a></Link>
                     </li>
@@ -31,15 +39,12 @@ export default function Layout({ children }) {
                     <li className="item">
                         <Link href="/contact"><a href="#">Contact</a></Link>
                     </li>
-                </ul>
+                </div>
             </nav>
-            <div className="language-setter">
+            <div className={isOpen === true ? "language-setter language-visible" : "language-setter"}>
                 <div className="setter" onClick={toggleLanguage}>
                     <Image alt="Language Flag" src={`/images/${language === 'en' ? 'english.png' : 'spanish.png'}`} object-fit='cover' layout='fill' />
                 </div>
-            </div>
-            <div className="burger">
-
             </div>
         </header>
 
@@ -98,8 +103,8 @@ export default function Layout({ children }) {
 
             .burger {
                 display:none;
+                cursor:pointer;
             }
-
 
             footer {
                 width:100vw;
@@ -108,6 +113,62 @@ export default function Layout({ children }) {
                 place-items:center;
                 background:black;
                 color:white;
+            }
+
+            @media screen and (max-width:1200px) {
+                .app-header {
+                    display:flex;
+                    width:100vw;
+                    justify-content:space-between;
+                }
+
+                .navigation {
+                    display:none;
+                    transition: all .4s ease;
+                    position:absolute;
+                    top:10vh;
+                    left:0;
+                    background:#ffffff;
+                    border-radius:1rem;
+                    border:1px solid black;
+                    box-shadow:2px 2px 5px #ccc;
+                    height:15rem;
+                    align-items:center;
+                    justify-content:space-evenly;
+                    margin:1rem;
+                    right:0;
+                }
+
+                .item {
+                    font-weight:500;
+                    font-size:1.4rem;
+                }
+
+                .item a {
+                    border-color:transparent;
+                }
+
+                .list {
+                    height:100%;
+                    flex-direction:column;
+                    justify-content:space-evenly;
+                }
+
+                .burger {
+                    display:block;
+                }
+
+                .language-setter {
+                    margin:0;
+                }
+
+                .visible {
+                    display:flex;
+                }
+
+                .hidden {
+                    display:none;
+                }
             }
         `}</style>
         </>
